0f8bf411c26ca74e14055a9aa64d2a83fab9db6c
trol <andrewtheado@uafools.org>
2022-10-12 20:49:35 -0400

    i am speed

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
         int count=0;
         int p;
         Scanner s = new Scanner(System.in);
@@ -10,17 +15,33 @@ public class Main {
         p = s.nextInt();
         System.out.println("how many threads");
         int t = s.nextInt();
+        Runtime rt = Runtime.getRuntime();
 
-        Thread commit =new Thread(){
-           public void run(){
-               while(count<p) {
-                   try {
-                       Runtime.getRuntime().exec("git commit --allow-empty -m \"Threaded Commit\"");
-                   } catch (IOException e) {
-                       throw new RuntimeException(e);
-                   }
-               }
-           }
-        };
+        //prr = rt.exec("git commit --allow-empty -m \"i am speed\"");
+
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(t);
+
+        executor.submit(() -> {
+            for(int i=0;i<=p/t;i++) {
+                try {
+                    Process prr = rt.exec("git add .");
+                    printResults(prr);
+                    //Process pr = rt.exec("git commit --allow-empty -m \"i am speed\"");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+
+
+
+    }
+    public static void printResults(Process process) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line = "";
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
     }
 }
